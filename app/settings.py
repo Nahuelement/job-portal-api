9005eb39c63893e76cc43d106c6c38360557c51a
Nahuelement <nahuel.perugi@gmail.com>
2022-11-06 22:46:21 -0300

    update

--- app/settings.py
+++ app/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv.read_dotenv()
+# dotenv.read_dotenv()
 
 
 
