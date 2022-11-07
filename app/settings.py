e45094ddb4d3cdfe0c1393a848d93b1baccfb86e
Nahuelement <nahuel.perugi@gmail.com>
2022-11-07 15:47:15 -0300

    depy

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['portal-trabajos.herokuapp.com/','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['nahuel-jobs.herokuapp.com','localhost', '127.0.0.1']
 
 
 
